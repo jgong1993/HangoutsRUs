77c310ebdef9f28d47c338c50c1c768e67d8b89d
Kathy Luu <k3luu@ucsd.edu>
2016-02-10 07:03:16 +0000

    took out static

--- routes/addGroup.js
+++ routes/addGroup.js
@@ -7,8 +7,6 @@ exports.addNewGroup = function(req, res) { 
 	var name = req.query.name;
 	var description = req.query.description;
 	var obj = { "name": name, "description": description }
-	console.log("req.query.name: " +req.query.name);
-	console.log("req.query.description: " +req.query.description);
 	console.log("name: " +name);
 	console.log("description: " +description);
 	console.log("obj: " +obj);
