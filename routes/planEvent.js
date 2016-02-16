18fad73814aa41d46d0dc6664d1df9868a6b93ab
aidaroff <aaidarov@ucsd.edu>
2016-02-16 01:15:47 -0800

    Select from existing groups when making new event

--- routes/planEvent.js
+++ routes/planEvent.js
@@ -1,5 +1,11 @@
 var data = require("../events.json");
+var groups = require("../groups.json");
 
 exports.planEventView = function(req, res) {
 	res.render('planEvent', data);
+}
+
+
+exports.planEventView = function(req, res) {
+	res.render('planEvent', groups);
 }
