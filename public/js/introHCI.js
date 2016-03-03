9d5e18621218d021fd87751c473e67f75506fa31
aidaroff <aaidarov@ucsd.edu>
2016-03-02 22:39:10 -0800

    added analytics

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -3,11 +3,16 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
-})
+});
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	console.log("Javascript connected!");
+	// your code here
+	$('.photos').click(photosClick);
+}
+
+function photosClick() {
+	ga('send', 'event', 'photos', 'click');
 }
